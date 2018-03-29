4ea6f3b08be7481383edda961e539db794c622aa
b6029476 <b6029476@my.shu.ac.uk>
2018-03-29 15:53:55 +0100

    First Bootstrap created and tested

--- hrSystem/grails-app/init/hrsystem/BootStrap.groovy
+++ hrSystem/grails-app/init/hrsystem/BootStrap.groovy
@@ -1,9 +1,17 @@
-package hrsystem
+package com.jchambers
 
 class BootStrap {
-
-    def init = { servletContext ->
-    }
+def init = { servletContext ->
+def employee1=new Employee (fullName:'James Sharbucho',
+dateOfBirth:new Date('09/11/1996'),
+residence:'Sheffield',
+hourlyRate: 7.50,
+employeeID:'TR8R',
+dateEmployed:new Date('09/11/2018'),
+taxCode:'TX935',
+contract:'Full-Time').save()
+}
     def destroy = {
     }
+
 }
